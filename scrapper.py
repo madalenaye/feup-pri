c7ed958eaf7c34bc7b82447ab12f6f198ba38c9d
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-10-01 12:22:42 +0100

    fetch list episodes

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import json 
-
+episodes_url ='https://bulbapedia.bulbagarden.net/wiki/List_of_animated_series_episodes'
+def get_list_episodes(url):
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    with urllib.request.urlopen(req) as url:
+        soup = BeautifulSoup(url, 'html.parser')
+        fetch = soup.find_all('tr',style="text-align:center; background:#FFFFFF")
+        return [i.contents[1].text.strip() for i in fetch]
 
 def fetchText(url):
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
