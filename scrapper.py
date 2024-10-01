3165a7e8c988820a3e4a2badb4c3115372fe187f
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-10-01 22:05:18 +0100

    added table reader for eps

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import urllib.request
 import json 
 
-episodes_url = 'https://bulbapedia.bulbagarden.net/w/api.php?action=parse&format=json&page=List_of_animated_series_episodes'
 api_url= 'https://bulbapedia.bulbagarden.net/w/api.php?action=parse&format=json&page='
 
 def get_list_episodes(url):
@@ -39,4 +38,43 @@ def get_plot_from_episode(episode):
 
     return getPlot(soup)
 
-print(get_plot_from_episode("EP041"))
+def get_table(soup):
+    table = {}
+    tableHeader = soup.find("table", class_="roundy", style="float:right; display: table !important; background: #FFAAAA; width: 25%; margin-left: 5px; margin-bottom: 5px")
+    
+    ep_code = tableHeader.find("td", class_="roundy", width="25%",style="background: #AAFFAA;")
+    table["Episode Code"]=ep_code.text.strip()
+
+    title = tableHeader.find("big")
+    table["Name"]=title.text.strip()
+
+    subtitle = tableHeader.find("small")
+    table["Subtitle"]=subtitle.text.strip()
+
+    subtables = tableHeader.find_all("td", class_="roundy" ,style="background:#AAFFAA")
+    for subtable in subtables:
+        sub_header = subtable.find("b")
+        if sub_header:
+            table[sub_header.text.strip()]={}
+            for th in subtable.find_all("th"):
+                if th:
+                    if(th.find_next_sibling()):
+                        table[sub_header.text.strip()][th.text.strip()]= th.find_next_sibling().text.strip()
+    ### I dont consider adicional credits, we need another approach otherwise
+    return table          
+            
+        
+i=0
+#for episode in get_list_episodes(api_url+'List_of_animated_series_episodes'):
+ #   if i==28:
+  #      break
+  #  print(get_plot_from_episode(episode))
+  #  print("-----------------------------")
+   # i-=1
+for episode in get_list_episodes(api_url+'List_of_animated_series_episodes'):
+    if i<=30:
+        print(get_table(BeautifulSoup(fetchText(api_url+episode),"html.parser")))
+        print("-----------------------------")
+    else:
+        break;
+    i+=1
