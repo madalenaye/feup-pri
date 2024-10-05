c0d9f9e2cc1ff4f0fe4e46822045cf47b9300d54
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-10-05 16:19:12 +0100

    added character table scrapper

--- characters_scrapper.py
+++ characters_scrapper.py
@@ -25,13 +25,8 @@ def get_list_characters(api_url,page):
             ep = cells[1].find("a").get("href").split("/")[-1]
             role = cells[2].text.strip()
             list_of_characters.append((character,character_page,ep,role))
-    return list_of_characters    
-            
+    return list_of_characters
         
-    
-    
-
-
 # Gets the character text from the html for a character
 # Arg api_url: string url
 # Arg name: string name of the character
@@ -48,9 +43,37 @@ def get_character_character(api_url,name):
             if(sibling.name == "p"):
                 text += sibling.text.strip() 
             sibling = sibling.find_next_sibling()
-            
+    else:
+        print("Character section not found for given character: "+name)
+        text = ""
     return text
 
+def get_character_table_info(api_url,name):
+    table = {}
+    html = fetchText(api_url+name)
+    soup = BeautifulSoup(html,"html.parser")
+    character_table_section = soup.find("big")
+    character_table = character_table_section.find_parent("table")
+    if character_table:
+        rows = character_table.find_all("tr")
+        name = rows[0].find("big").text.strip()
+        table["Name"]=name
+        japanese_name = rows[0].find("span")
+        if japanese_name:
+            table["Japanese Name"]=japanese_name.text.strip() + " " +"("+japanese_name.find_next_sibling("i").text.strip()+")"
+        for row in rows[1:]:
+            key = row.find("th")
+            value = row.find("td")
+            if(key and value):
+                table[key.text.strip()] = value.text.strip()
+    else:
+        print("Table not found for given character: "+name)
+        character_table = {}
 
+    return table
+    
+        
+    
 #print(get_character_character(api_url,"Ash_Ketchum"))
-print(get_list_characters(api_url,"List_of_animated_series_characters#Original_series"))
+#print(get_list_characters(api_url,"List_of_animated_series_characters#Original_series"))
+print(get_character_table_info(api_url,"Ash_Ketchum"))
--- series_scrapper.py
+++ series_scrapper.py
@@ -94,13 +94,19 @@ i=0
     print(get_plot_from_episode(episode))
     print("-----------------------------")
     i-=1
-for episode in get_list_episodes(api_url+'List_of_animated_series_episodes'):
-    if i<=30:
-        print(get_table(BeautifulSoup(fetchText(api_url+episode),"html.parser")))
-        print("-----------------------------")
-    else:
-        break;
-    i+=1
 '''
+for episode in get_list_episodes(api_url,'List_of_animated_series_episodes'):
+    try:
+        if i<=40:
+            print(get_table(BeautifulSoup(fetchText(api_url+episode),"html.parser")))
+            print("-----------------------------")
+        else:
+            break;
+        i+=1
+    except:
+        print("Error in episode: "+episode)
+        continue
+
+#print(get_table(BeautifulSoup(fetchText(api_url+'EP042'),"html.parser")))
 #print(get_characters(api_url,'EP002'))
 #print(get_list_episodes(api_url,'List_of_animated_series_episodes'))
