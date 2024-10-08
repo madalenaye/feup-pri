f04a2bd4db72d79193fb3559faf12e612d31251d
Madalena Ye <mads1040@gmail.com>
2024-10-08 12:10:10 +0100

    pokemon stats

--- pokemon_scrapper.py
+++ pokemon_scrapper.py
@@ -13,14 +13,15 @@ def get_list_pokemon(api_url, page):
         pokedex_entry = i.contents[1].text.strip()
         pokemon = i.contents[5].find('a').text.strip()
         pokemon_page = i.contents[3].find('a').get('href').split('/')[-1]
-        list_of_pokemon.append((pokedex_entry, pokemon, pokemon_page))
+        pokemon_types = [i.contents[7].find('a').text.strip(), i.contents[9].find('a').text.strip() if len(i.contents) > 9 else None]
+        list_of_pokemon.append((pokedex_entry, pokemon, pokemon_page, pokemon_types))
     return list_of_pokemon
 
 def get_pokemon_blurb(html, name):
     text = ""
     soup = BeautifulSoup(html, "html.parser")
-    beforeBlurb = soup.select('div.mw-parser-output > table')[1]
-    sibling = beforeBlurb.find_next_sibling()
+    before_blurb = soup.select('div.mw-parser-output > table')[1]
+    sibling = before_blurb.find_next_sibling()
     if sibling.name == "p":
         while sibling.name != "div":
             if sibling.name == "p":
@@ -36,8 +37,8 @@ def get_pokemon_blurb(html, name):
 def get_pokemon_biology(html, name):
     text = ""
     soup = BeautifulSoup(html, "html.parser")
-    beforeBiology = soup.find("div", class_='thumb tleft')
-    biology = beforeBiology.find_next_sibling()
+    before_biology = soup.find("div", class_='thumb tleft')
+    biology = before_biology.find_next_sibling()
     if biology.name == "p":
         while biology.name != "h3":
             if biology.name == "p":
@@ -50,18 +51,42 @@ def get_pokemon_biology(html, name):
         raise Exception("Biology section not found for given pokemon: " + name)
     return text
 
-#def get_pokemon_stats(html, name):
+def get_pokemon_stats(html, name):
+    soup = BeautifulSoup(html, "html.parser")
+    pokemon_stats = []
+    pokemon_abilities = []
+    table = soup.select('div.mw-parser-output > table')[1]
+    table_body = table.find('tbody')
+    ability_row = table_body.contents[4].find_all('td')
+    for i in ability_row:
+        if i.find('a') is not None and "style" not in i.attrs:
+            pokemon_abilities.append(i.find('a').text.strip())
+    image_row = table_body.contents[0].find_all('td')
+    for i in image_row:
+        a = i.find('a', class_="image")
+        if a and ("style" not in i.attrs):
+            img = a.find('img').get('src')
+            pokemon_stats.append(img)
+            break
+    pokemon_stats.append(pokemon_abilities)
+    return pokemon_stats
+
+
+
+      
+def get_pokemon_evolution(html, name):
     
-#def get_pokemon_evolution(html, name):
+    return
 
 
-'''def main():
+def main():
     #print(get_list_pokemon(api_url, 'List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number'))
     all = get_list_pokemon(api_url, 'List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number')
-    test = all[1022][2]
+    test = all[11][2]
     url = fetchText(api_url+test)
-    test1 = get_pokemon_biology(url, 'Bulbasaur')
+    test1 = get_pokemon_stats(url, 'Bulbasaur')
     print(test1)
 
 main()
-''' 
+
+#print(get_list_pokemon(api_url, 'List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number'))
