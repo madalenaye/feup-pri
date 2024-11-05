0421258f99d6676c904f63b1f474a2cbfd2c6a75
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-11-05 21:09:00 +0000

    fix to schema

--- m2/startup.sh
+++ m2/startup.sh
@@ -1,6 +1,8 @@
 #Startup with docker
 docker run -p 8983:8983 --name pokemon_the_series -v ${PWD}:/data -d solr:9 solr-precreate episodes
 
+sleep 3
+
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema.json http://localhost:8983/solr/episodes/schema
 
 curl -X POST -H 'Content-type:application/json' \
