37da550fddf88b575d511ccd8a264ff2edcaed43
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-11-05 18:52:12 +0000

    process.py

---
+++ m2/process.py
@@ -0,0 +1,20 @@
+import json 
+import pandas as pd
+
+def convert_to_iso(episodes):
+    for episode in episodes:
+        print(pd.to_datetime(episode["first broadcast japan"], unit='ms').isoformat())
+        if(episode["first broadcast japan"]):
+            episode["first broadcast japan"] = pd.to_datetime(episode["first broadcast japan"], unit='ms').isoformat()
+
+        if(episode["first broadcast united states"]):
+            episode["first broadcast united states"] = pd.to_datetime(episode["first broadcast united states"], unit='ms').isoformat()
+    return episodes;
+
+with open('data/docs/new_episodes.json') as file:
+    episodes = json.load(file)
+
+episodes = convert_to_iso(episodes)
+
+with open('data/docs/new_episodes.json', 'w') as file:
+    json.dump(episodes, file, indent=4)
