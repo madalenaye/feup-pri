c7d87b6862a27022ede6827b7497acc44f796d66
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-11-05 11:44:12 +0000

    ???

--- m2/startup.sh
+++ m2/startup.sh
@@ -1,7 +1,4 @@
 #Startup with docker
-docker stop pokemon_the_series
-docker rm pokemon_the_series
-
 docker run -p 8983:8983 --name pokemon_the_series -v ${PWD}:/data -d solr:9 solr-precreate episodes
 
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema.json http://localhost:8983/solr/episodes/schema
