e55bf1395455f7ad017f0854eeeff24d20c8b2f4
Tomás Vicente <93838997+tomasvicente3@users.noreply.github.com>
2024-11-05 11:28:19 +0000

    fixed startup.sh

--- m2/startup.sh
+++ m2/startup.sh
@@ -1,8 +1,13 @@
 #Startup with docker
+docker stop pokemon_the_series
+docker rm pokemon_the_series
+
 docker run -p 8983:8983 --name pokemon_the_series -v ${PWD}:/data -d solr:9 solr-precreate episodes
 
-curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema.json http://localhost:8983/solr/pokemon/schema
+sleep 3
+
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema.json http://localhost:8983/solr/episodes/schema
 
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @new_episodes.json \
-    http://localhost:8983/solr/pokemon/update?commit=true
+    http://localhost:8983/solr/episodes/update?commit=true
