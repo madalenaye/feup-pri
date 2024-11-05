71e157081975089a27c90fac9c8663638162244d
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-11-05 12:06:03 +0000

    test

--- m2/merge_docs.py
+++ m2/merge_docs.py
@@ -24,18 +24,27 @@ def merge_humans(episodes, characters):
     for episode_data in episodes:
         episode_data["human characters"] = list(map(lambda x: merge_single_human(x, characters), episode_data["human characters"]))
 
-##def find_pokemon(code,pokemons):
-  #  for pokemon in pokemons:
-   #     if pokemon["Pokemon Code"] == code:
-   #         return pokemon
-    #return None    
-#def merge_pokemons(episodes,pokemon):
- #   for episode_data in episodes:
-  #      episode_data["pokemon characters"] = list(map(lambda x: merge_single_pokemon(x, pokemon), episode_data["pokemon characters"]))
-with open("data/episodes_final.json", "r") as episode_file, open("data/characters_final.json") as character_file:
+def find_pokemon(code,pokemons):
+    for pokemon in pokemons:
+        if pokemon["Pokemon Page"] == code:
+            return pokemon
+    return None    
+def merge_single_pokemon(pokemon, pokemons):
+    all_pokemon = find_pokemon(pokemon["code"], pokemons)
+    if(all_pokemon is None):
+        return pokemon
+    for attr in all_pokemon.keys():
+        pokemon[attr] = all_pokemon[attr]
+    return pokemon
+def merge_pokemons(episodes,pokemon):
+    for episode_data in episodes:
+        episode_data["pokemon characters"] = list(map(lambda x: merge_single_pokemon(x, pokemon), episode_data["pokemon characters"]))
+with open("data/docs/episodes_final.json", "r") as episode_file, open("data/docs/characters_final.json") as character_file,open("data/docs/pokemons_final.json") as pokemon_file:
     episodes = json.load(episode_file)
     characters = json.load(character_file)
+    pokemons = json.load(pokemon_file)
 
 merge_humans(episodes, characters)
-with open("new_episodes.json", "w") as new_file:
+merge_pokemons(episodes, pokemons)
+with open("data/docs/new_episodes.json", "w") as new_file:
     json.dump(episodes, new_file, indent=4)
