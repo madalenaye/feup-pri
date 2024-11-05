57b0d13b05d840ebe2cfd5e37e7fed5caf94e782
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-11-05 21:10:21 +0000

    update bash file afonso

--- m2/startup_afonso.sh
+++ m2/startup_afonso.sh
@@ -7,13 +7,13 @@ cd ~/solr-9.7.0/bin
 
 sleep 5
 
-./solr delete -c pokemon
-./solr create -c pokemon
+./solr delete -c episodes
+./solr create -c episodes
 
 cd ~/feup-pri/m2
 
-curl -X POST -H 'Content-type:application/json' --data-binary @./schema.json http://localhost:8983/solr/pokemon/schema
+curl -X POST -H 'Content-type:application/json' --data-binary @./schema.json http://localhost:8983/solr/episodes/schema
 
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @new_episodes.json \
-    http://localhost:8983/solr/pokemon/update?commit=true
+    http://localhost:8983/solr/episodes/update?commit=true
