c6ab906c47c33a1fa5b5865e35310652c3248ff9
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-11-06 15:36:24 +0000

    some index for names (auto-complete+sound similarity)

--- m2/startup.sh
+++ m2/startup.sh
@@ -3,8 +3,8 @@ docker run -p 8983:8983 --name pokemon_the_series -v ${PWD}:/data -d solr:9 solr
 
 sleep 3
 
-curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema.json http://localhost:8983/solr/episodes/schema
-
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema2.json http://localhost:8983/solr/episodes/schema
+sleep 2
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @./data/docs/new_episodes.json \
     http://localhost:8983/solr/episodes/update?commit=true
