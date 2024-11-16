b2d665a6e873f0f29acdb3435bf4e59d34d926f9
Onso37 <afonso.vo@gmail.com>
2024-11-16 14:36:53 +0000

    try new synonyms

--- m2/synonyms.py
+++ m2/synonyms.py
@@ -1,14 +1,31 @@
 import nltk
 from nltk.corpus import wordnet
+import pandas as pd
 
-nltk.download('averaged_perceptron_tagger', quiet=True)
+nltk.download('averaged_perceptron_tagger_eng', quiet=True)
 nltk.download('wordnet', quiet=True)
+nltk.download('punkt_tab', quiet=True)
 
-words = ["win", "lose", "victory", "defeat", "battle", "friend", "anger", "angry", "sad", "catch", "group", "help", "tries", "run"]
+pokemon_df = pd.read_json("data/docs/pokemons_final.json")
+biologies = [pokemon["Biology"] for index, pokemon in pokemon_df.iterrows()]
 
-for word in words:
-    synonyms = set()
-    for synonym in wordnet.synsets(word):
-        for name in synonym.lemma_names():
-            synonyms.add(name)
-    print(list(synonyms))
+used_words = set()
+syn_list = []
+
+for biology in biologies:
+    adjectives = [word.lower() for word, pos in nltk.pos_tag(nltk.word_tokenize(biology)) if pos in ['JJ', 'JJR', 'JJS'] and word.lower() not in used_words]
+
+    for adj in adjectives:
+        syns = [adj]
+        for syn in wordnet.synsets(adj):
+            for lemma in syn.lemma_names():
+                if lemma.lower() != adj:
+                    syns.append(lemma)
+        used_words.add(adj)
+        if len(syns) > 2:
+            syn_list.append(syns)
+
+with open("data/synonyms.txt", "w") as synfile:
+    for l in syn_list:
+        synfile.write(" ,".join(l) + '\n')
+
