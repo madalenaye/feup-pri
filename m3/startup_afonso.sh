084551fdfe182ae261f59db9d6bf836cd56a63fe
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-12-03 10:49:03 +0000

    start embeddings

---
+++ m3/startup_afonso.sh
@@ -0,0 +1,24 @@
+#A PARTE DE INICIAR O SOLR NESTE FICHEIRO É SÓ PARA O MEU PORTÁTIL SEM DOCKER
+
+cd ~/solr-9.7.0/bin
+
+./solr stop --all
+./solr start &
+
+sleep 10
+
+./solr delete -c episodes
+./solr create -c episodes
+
+
+cd ~/feup-pri/m3
+cp data/synonyms.txt /home/onso/solr-9.7.0/server/solr/episodes/conf/synonyms.txt
+cp data/pokemon_synonyms.txt /home/onso/solr-9.7.0/server/solr/episodes/conf/pokemon_synonyms.txt
+
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary @data/semantic_schema.json http://localhost:8983/solr/episodes/schema
+
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @data/docs/semantic_episodes.json \
+    http://localhost:8983/solr/episodes/update?commit=true
