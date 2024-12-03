1c2986e9d5263e75b9e50d445459a55f02bbd307
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-12-03 11:55:04 +0000

    backend for the project

---
+++ m3/backendtutorial.md
@@ -0,0 +1,7 @@
+1. Install django using pip 
+
+```py pip install django
+
+2. Create views/request handlers in views.py
+3. Add import of the view/request handler in urls.py
+4. add endpoint associated with that view/request handler 
---
+++ m3/startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# create semantic episodes file
+cat data/docs/new_episodes.json | python3 get_embeddings.py > data/semantic_episodes.json
+# Startup with Docker
+docker run -p 8983:8983 --name pokemon_the_series -v ${PWD}/data:/data -d solr:9 solr-precreate episodes
+
+# Wait for Solr to start
+echo "Waiting for Solr to start..."
+until $(curl --output /dev/null --silent --head --fail http://localhost:8983/solr/episodes/admin/ping); do
+    printf '.'
+    sleep 2
+done
+
+echo "Solr is up and running."
+
+docker cp data/synonyms.txt pokemon_the_series:/var/solr/data/episodes/conf/synonyms.txt
+docker cp data/pokemon_synonyms.txt pokemon_the_series:/var/solr/data/episodes/conf/pokemon_synonyms.txt
+
+# Post the schema
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/semantic_schema.json http://localhost:8983/solr/episodes/schema
+
+# Wait for a moment to ensure the schema is applied
+sleep 2
+
+# Post the documents
+docker exec -it pokemon_the_series bin/solr post -f -c episodes /data/out/semantic_episodes.json
+
+docker restart pokemon_the_series
+echo "Schema and documents have been posted."
