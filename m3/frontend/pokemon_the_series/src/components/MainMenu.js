282a60cf9fcf6fe2998ea1f345de99c149a04f1c
HaoChang Fu <fuhaochang664961423@hotmail.com>
2024-12-04 23:32:55 +0000

    adjustments to the frontend

--- m3/frontend/pokemon_the_series/src/components/MainMenu.js
+++ m3/frontend/pokemon_the_series/src/components/MainMenu.js
@@ -1,5 +1,6 @@
-import pokeball from "../images/pokeball.png"
+import pokedex from "../images/pokedex.png"
 import johtoBadges from "../images/johto-badges.png"
+import badge from "../images/badge.webp"
 import Video from "./Video.js"
 import backGroundLoop from "../videos/backgroundLoop.mp4"
 export default function MainMenu() {
@@ -7,14 +8,14 @@ export default function MainMenu() {
     return (
       <div className="main-menu h-screen">
         <Video src={backGroundLoop} ariaLabel="Background video blue with a rotating pokeball bymbol" classList={["opacity-80", "-z-10" ,"absolute","video-background"]}/>
-        <div className="top-bar">
-            <h1>Header</h1>
+        <div className="top-bar flex-row content-center justify-items-end">
+            <HeaderLogo src={badge}/>
         </div>
         <div className="main-menu-item1 flex-row flex-wrap content-center justify-items-center">
-            <Pokeball src={pokeball} classList={["hover:bg-gray-100"]}/>
+            <Pokedex src={pokedex}  classList={["select"]}/>
         </div>
         <div className="main-menu-item2  flex-row flex-wrap content-center justify-items-center">
-            <Badges src={johtoBadges}/>
+            <Badges src={johtoBadges} classList={["select"]}/>
         </div>
         <footer className="footer">
             <h1>All rights reserved</h1>
@@ -23,16 +24,22 @@ export default function MainMenu() {
     );
 }
 
-
-function Pokeball({src,classList}){
+function HeaderLogo({src,classList=[]}){
+    const classlist = [...classList];
+    return(
+        <img className={classlist.join(" ")} id="header-logo" src={src} alt="Badge logo"/>
+    )
+}
+function Pokedex({src,classList=[]}){
     const classlist = [...classList]
     return(
-        <img className={classlist.join(" ")} id="pokeball" src={src} alt="Pokeball logo"/>
+        <img className={classlist.join(" ")} id="pokedex" src={src} alt="Pokeball logo"/>
     );
 }
-function Badges ({src}){
+function Badges ({src,classList=[]}){
+    const classlist = [...classList]
     return(
-        <img className="" id="badge" src={src} alt="Johto Badges image"/>
+        <img className={classlist.join(" ")} id="badge" src={src} alt="Johto Badges image"/>
     );
 }
   
