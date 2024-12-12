7a1cb18fe2429110da90a3caefa0504b9071395e
Madalena Ye <mads1040@gmail.com>
2024-12-12 09:31:17 +0000

    mlt

--- m3/mlt.py
+++ m3/mlt.py
@@ -9,6 +9,7 @@ mlt = {
         "mlt.fl": "plot, title, major_events",
         "mlt.mintf": 2,
         "mlt.mindf": 6,
+        "mlt.qf": "plot^8 title^2 major_events^5",
         "sort": "score desc",
         "rows": 5
     }
@@ -31,26 +32,24 @@ def fetch_solr_results(episode, solr_uri, collection):
     docs = response.json().get("response", {}).get("docs", [])
     
     if len(docs) > 0:
-        if os.path.exists(mlt["results"]):
-            with open(mlt["results"], "r", encoding="utf-8") as file:
-                results = json.load(file)
-        else:
-            results = []
-        
+        result = []
         results.append({
             "id": episode["id"],
             "related": [{"id": docs[i]["id"]} for i in range(len(docs))]
         })
-        with open(mlt["results"], "w", encoding="utf-8") as file:
-            json.dump(results, file, indent=2)
-    
-    return docs
+        return result
 
 if __name__ == "__main__":
     with open("./data/docs/new_episodes.json", "r") as file:
         episodes = json.load(file)
     
-    for episode in episodes:
-        fetch_solr_results(episode, "http://localhost:8983/solr", "episodes")
     
+    results = []
+        
+    for episode in episodes:
+        result = fetch_solr_results(episode, "http://localhost:8983/solr", "episodes")
+        results.append(result)
+    
+    with open(mlt["results"], "w", encoding="utf-8") as file:
+        json.dump(results, file, indent=2)
     print("Done!")
